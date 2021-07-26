85af8f542ff72734be62aa8d15e624d429b1447b
Cintia Ribeiro <cintiarbrandao@gmail.com>
2021-07-26 16:56:23 -0300

    Expo e styled components

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+import themes from './src/global/styles/themes';
+
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+
+  return (
+    <ThemeProvider theme={themes}>
+      
+      <Dashboard/>
+
+    </ThemeProvider>
+    
+  );
+}
+
+
